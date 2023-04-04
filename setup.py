daa444d43e633dccb77a43869a64992eda277197
Huang Po-Wei <71061276+georgepwhuang@users.noreply.github.com>
2023-04-05 03:38:23 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bnn',
+    version='',
+    packages=['bnn', 'bnn.model', 'bnn.metrics', 'bnn.trainer', 'bnn.datamodule'],
+    url='',
+    license='',
+    author='georgehuang',
+    author_email='',
+    description=''
+)
