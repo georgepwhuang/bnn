Correct error in KL divergence

bnn/layers/BBBConv2d.py
@@ -70,8 +70,8 @@ class BBBConv2d(nn.Module):
         else:
             return act_mu
 
-    def kl_loss(self):
-        kl = KL_DIV(self.prior_mu, self.prior_sigma, self.W_mu, self.W_sigma)
+    def kl_loss(self, num_batches):
+        kl = KL_DIV(self.prior_mu, self.prior_sigma, self.W_mu, self.W_sigma, num_batches)
         if self.use_bias:
-            kl += KL_DIV(self.prior_mu, self.prior_sigma, self.bias_mu, self.bias_sigma)
+            kl += KL_DIV(self.prior_mu, self.prior_sigma, self.bias_mu, self.bias_sigma, num_batches)
         return kl

bnn/model/template/bayesian.py
@@ -18,7 +18,7 @@ class BayesianClassifier(BaseClassifier):
         klloss = 0
         for module in self.model.modules():
             if hasattr(module, 'kl_loss'):
-                klloss = klloss + module.kl_loss()
+                klloss = klloss + module.kl_loss(len(self.trainer.train_dataloader))
         self.log('train_kldiv_loss', klloss)
         total_loss = celoss + self.get_beta(batch_idx, len(self.trainer.train_dataloader), self.current_epoch,
                                             self.trainer.max_epochs) * klloss
@@ -33,7 +33,7 @@ class BayesianClassifier(BaseClassifier):
         klloss = 0
         for module in self.model.modules():
             if hasattr(module, 'kl_loss'):
-                klloss = klloss + module.kl_loss()
+                klloss = klloss + module.kl_loss(len(self.trainer.train_dataloader))
         self.log('val_kldiv_loss', klloss)
         total_loss = celoss + self.get_beta(batch_idx, len(self.trainer.val_dataloaders), self.current_epoch,
                                             self.trainer.max_epochs) * klloss
@@ -50,14 +50,6 @@ class BayesianClassifier(BaseClassifier):
         output = self(data)
         celoss = F.cross_entropy(output, corrected_label)
         self.log('test_cross_entropy_loss', celoss, on_epoch=True)
-        klloss = 0
-        for module in self.model.modules():
-            if hasattr(module, 'kl_loss'):
-                klloss = klloss + module.kl_loss()
-        self.log('test_kldiv_loss', klloss)
-        total_loss = celoss + self.get_beta(batch_idx, len(self.trainer.test_dataloaders), self.current_epoch,
-                                            self.trainer.max_epochs) * klloss
-        self.log('test_total_loss', total_loss)
         logits = F.softmax(output, -1)
         self.test_metrics(logits, corrected_label)
         self.log_scalars(self.test_metrics)

bnn/util/util.py
@@ -1,5 +1,5 @@
 import torch
 
-def calculate_kl(mu_q, sig_q, mu_p, sig_p):
-    kl = 0.5 * (2 * torch.log(sig_p / sig_q) - 1 + (sig_q / sig_p).pow(2) + ((mu_p - mu_q) / sig_p).pow(2)).mean()
+def calculate_kl(mu_q, sig_q, mu_p, sig_p, num_batches):
+    kl = 0.5 * (2 * torch.log(sig_p / sig_q) - 1 + (sig_q / sig_p).pow(2) + ((mu_p - mu_q) / sig_p).pow(2)).sum() / num_batches
     return kl